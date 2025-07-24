0222dfa0aecacc3c07b573a860a6f4c22c00584f
yashswi23 <yashswi2314@gmail.com>
2025-07-24 11:58:50 +0530

    Modify Bash Related Commands

--- src/components/MarkdownSanitized.jsx
+++ src/components/MarkdownSanitized.jsx
@@ -1,20 +1,24 @@
-// import React, { useContext } from 'react'
-// import { markDownContext } from "../context/markDownContext";
-// const MarkdownSanitized = () => {
-//     const {getMarkdownAshtmloutput} = useContext(markDownContext);
-//   return (
-//     <div className='flex-1 p-6 bg-white border-l border-gray-300'>
-//         <h3 className='text-xl font-semibold mb-4'>MarkdownOutput</h3>
-//         <div
-//         className="prose prose-lg max-w-none border p-4 rounded-lg bg-gray-50 min-h-[80vh] shadow-sm"
-//         dangerouslySetInnerHTML={{ __html: getMarkdownAshtmloutput() }}
-//       />
-      
-//     </div>
-//   )
-// }
 
-// export default MarkdownSanitized
+// import React, { useContext } from "react";
+// import { markDownContext } from "../context/markDownContext";
+
+// const MarkdownSanitized = () => {
+//   const { getMarkDownAsHTMLOutput } = useContext(markDownContext);
+
+//   return (
+//     <div className="flex-1 p-6 bg-white overflow-y-auto">
+//       <h3 className="text-xl font-semibold mb-4">Markdown Preview</h3>
+//       <div
+//          className="prose prose-lg max-w-none border p-4 rounded-lg 
+//              bg-gray-50 dark:bg-gray-800 dark:prose-invert 
+//              min-h-[75vh] shadow-sm"
+//         dangerouslySetInnerHTML={{ __html: getMarkDownAsHTMLOutput() }}
+//       />
+//     </div>
+//   );
+// };
+
+// export default MarkdownSanitized;
 
 import React, { useContext } from "react";
 import { markDownContext } from "../context/markDownContext";
@@ -23,12 +27,14 @@ const MarkdownSanitized = () => {
   const { getMarkDownAsHTMLOutput } = useContext(markDownContext);
 
   return (
-    <div className="flex-1 p-6 bg-white overflow-y-auto">
-      <h3 className="text-xl font-semibold mb-4">Markdown Preview</h3>
+    <div className="flex-1 p-6 bg-white dark:bg-[#1e1e2f] overflow-y-auto">
+      <h3 className="text-xl font-semibold mb-4 text-gray-800 dark:text-white">
+        Markdown Preview
+      </h3>
+
       <div
-         className="prose prose-lg max-w-none border p-4 rounded-lg 
-             bg-gray-50 dark:bg-gray-800 dark:prose-invert 
-             min-h-[75vh] shadow-sm"
+        className="prose prose-lg max-w-none border p-4 rounded-lg bg-gray-50 
+                   dark:bg-gray-800 dark:prose-invert min-h-[75vh] shadow-sm"
         dangerouslySetInnerHTML={{ __html: getMarkDownAsHTMLOutput() }}
       />
     </div>
@@ -36,3 +42,4 @@ const MarkdownSanitized = () => {
 };
 
 export default MarkdownSanitized;
+
